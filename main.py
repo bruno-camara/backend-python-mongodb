1c19720b068ae0cf7f9760d9de160c4ab6f65aec
bruno-camara <brunao.camara@gmail.com>
2023-04-10 15:12:47 +0200

    add feature age pyramid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from pymongo import MongoClient
+import pprint
 
 def collection(uri):
     client = MongoClient(uri, 27017) # client to the mongod instance
@@ -17,13 +18,15 @@ def load(collection, datapath="data.json"):
 # Create client, database and collection
 coll = collection("mongodb://localhost:27017/")
 
-# load database
+# Load database
 load(coll, "data.json.codechallenge.janv22.RHOBS.json")
 
 # Count the number of men and women
 print("Nombre de femmes:", coll.count_documents({"sex": "F"}))
 print("Nombre d'hommes:", coll.count_documents({"sex": "M"}))
 
+
+# Get company by the number of employees
 def get_company_number_employees(collection, number_employees):
     companies = collection.aggregate([
         {
@@ -38,4 +41,32 @@ def get_company_number_employees(collection, number_employees):
     ])
     return list(companies)
 
+print("\n\nGet companies by number of employees\n")
 print(get_company_number_employees(coll, 50))
+
+def age_pyramid(collection):
+    pyramid = collection.aggregate([
+        { "$addFields": {
+            "birthdate": {
+                "$toDate": "$birthdate"
+            }} 
+        },
+        { "$addFields":
+            { "age": { "$dateDiff": { "startDate": "$birthdate", "endDate": "$$NOW", "unit": "year" } } }
+        },
+        { "$bucket":
+            {
+                "groupBy" : "$age", 
+                "boundaries":[0,10,20,30,40,50,60,70,80,90,100], 
+                "default":"other", 
+                "output" : 
+                    {
+                        "total" : {"$sum" : 1}, 
+                    }
+            }
+        }
+    ])
+    return list(pyramid)
+
+print("\n\nAge Pyramid \n")
+print(age_pyramid(coll))
